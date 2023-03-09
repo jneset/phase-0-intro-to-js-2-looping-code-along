67146532e18bd9d58460e7a61fe3701331944162
Ethan Jones <ejones4224@gmail.com>
2023-03-09 11:53:18 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+    
+    writeCards(["Guadalupe", "Ollie", "Aki"]);
+
+  }
+
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
